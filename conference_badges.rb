fa45c340ddbd54b67349c2fd0fbec05624faf625
Brian Birdwell <bbirdwell@gmail.com>
2018-09-30 15:21:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,18 +4,21 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  @name_list = array.map! { |n| badge_maker(n) }
+  name_list = array.map! { |n| badge_maker(n) }
 end
 
 def assign_rooms(array)
-  @list = []
+  list = []
   array.each_with_index { |n, index|
-  @list << "Hello, #{n}! You'll be assigned to room #{index+1}!"
+  list << "Hello, #{n}! You'll be assigned to room #{index+1}!"
   }
- return @list
+ return list
 end
 
-def printer
-  puts @name_list
-  puts @list
+def printer(attendees)
+  attendees.each { |n| 
+    puts batch_badge_creator(n)
+    assign_rooms(n)
+    puts list
+  end
 end
