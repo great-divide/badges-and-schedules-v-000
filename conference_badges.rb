be75b4111cf90b1b6f8200096e1fe7612b580b35
Brian Birdwell <bbirdwell@gmail.com>
2018-09-30 15:15:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,11 @@ end
 def batch_badge_creator(array)
   array.map! { |n| badge_maker(n) }
 end
+
+def assign_rooms(array)
+  list = []
+  array.each_with_index do |n|
+  list << "Hello, #{n}! You'll be assigned to room #{n.index}!"
+ end
+ list
+end
