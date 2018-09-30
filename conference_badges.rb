a41f35fac20caade1b4449fbd78082c826189d9b
Brian Birdwell <bbirdwell@gmail.com>
2018-09-30 15:17:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 def assign_rooms(array)
   list = []
   array.each_with_index { |n, index|
-  list << "Hello, #{n}! You'll be assigned to room #{index}!"
+  list << "Hello, #{n}! You'll be assigned to room #{index+1}!"
   }
  return list
 end
